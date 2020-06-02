f7e244ccf135f721f45f232e8ee1ea3f63a6d66c
RajaAvinash22 <das.kumaravinash22@gmail.com>
2020-06-02 19:06:42 +0530

    29May task updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/funC';
-import Welcome from './components/classC';
-import Hello from './components/hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
 import ClassClick from './components/ClassClick'
 import FuncClick from './components/FuncClick'
 import EventBind from './components/EventBind'
@@ -17,27 +12,6 @@ import Form from './components/Form'
 function App() {
   return (
     <div className="App">
-      {/* when u have clue to pass properties u can pass by attributes */}
-      
-      {/* function components props */}
-      <Greet name="VIRAT" heroName="Chikoo">
-        <p>Hello Captain</p>
-      </Greet>
-      <Greet name="DHONI" heroName="Thalla" />
-
-      {/* class component props */}
-      <Welcome name="VIRAT" heroName="Chikoo" />
-      
-       <Welcome name="DHONI" heroName="Thalla" /> 
-
-      {/* JSX Basics */}
-       <Hello />
-
-       {/* State passing from one compo to another */}
-      <Message />
-
-      {/* setState method in detail */}
-      <Counter />
 
        {/* handling event with funcComponent */}
       <FuncClick />
